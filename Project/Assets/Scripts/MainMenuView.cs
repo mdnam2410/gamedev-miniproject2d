51652edc0d14101a215799722aa1b56c4dccac4f
Nam Mai <mdnam2410@gmail.com>
2022-10-19 21:54:35 +0700

    feat: Init project

---
+++ Project/Assets/Scripts/MainMenuView.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuView : MonoBehaviour
+{
+    [SerializeField] GameObject btnNewGame;
+    [SerializeField] GameObject btnVsBot;
+    [SerializeField] GameObject btnVsPlayer;
+
+    public void LoadScene()
+    {
+
+    }
+}
