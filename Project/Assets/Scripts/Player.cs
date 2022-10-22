7fed55ee08e7c1f5bbf8d2e2a8390a17912254db
nghiaump <nghiaump@gmail.com>
2022-10-22 23:21:05 +0700

    feat: Tạo prefabs Player và Bullet

--- Project/Assets/Scripts/Player.cs
+++ Project/Assets/Scripts/Player.cs
@@ -13,6 +13,19 @@ public class Player : MonoBehaviour
         Lose
     }
 
+    public enum FaceDirection
+    {
+        LeftRight,
+        RightLeft
+    }
+
+    public enum MovingState
+    {
+        None,
+        ToRight,
+        ToLeft
+    }
+
     public GameManager.GameTurn ownRole;
     public int hp;
     public Transform firePos;
@@ -26,7 +39,8 @@ public class Player : MonoBehaviour
     public Rigidbody2D rigid2D;
 
     public float movingSpeed = 10;
-    public bool isMoving;
+    public MovingState movingState;
+    public FaceDirection faceDirection;
 
     public Vector2 fireForce;
     public Player target;
@@ -35,7 +49,8 @@ public class Player : MonoBehaviour
     {
         this.hp = 100;
         this.currentStatus = PlayerStatus.Idle;
-        this.isMoving = false;
+        this.movingState = MovingState.None;
+        this.faceDirection = FaceDirection.LeftRight;
         this.rigid2D = GetComponent<Rigidbody2D>();
         this.InitFireForce();
 
@@ -73,42 +88,73 @@ public class Player : MonoBehaviour
 
     public virtual void UpdateMove()
     {
+        this.UpdateFaceDirection();
         this.MovingByKey();
         this.UpdateTankMovingAnimation();
 
     }
 
+    private void UpdateFaceDirection()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            this.ReverseDirectionByInput();
+        }
+    }
+
+    private void ReverseDirectionByInput()
+    {
+        this.transform.localScale = new Vector3(-this.transform.localScale.x, this.transform.localScale.y, this.transform.localScale.z);
+        this.faceDirection = (FaceDirection)(1 - this.faceDirection);
+    }
     private void MovingByKey()
     {
         if (Input.GetKey(KeyCode.D))
         {
             if (this.rigid2D.velocity.magnitude < 1)
                 this.rigid2D.AddForce(new Vector2(1, 0.6f));
-            this.isMoving = true;
+
+            this.movingState = MovingState.ToRight;
             
         }
         else if (Input.GetKey(KeyCode.A))
         {
             if (this.rigid2D.velocity.magnitude < 1)
                 this.rigid2D.AddForce(new Vector2(-1, 0.6f));
-            this.isMoving = true;
+
+            this.movingState = MovingState.ToLeft;
         }
         else
         {
-            this.isMoving = false;
+            this.movingState = MovingState.None;
         }
     }
 
     private void UpdateTankMovingAnimation()
     {
-        if (this.isMoving)
+        if (this.movingState != MovingState.None)
         {
             this.tankAnimator.SetBool("Moving", true);
+            if (this.isMovingToward())
+            {
+                this.tankAnimator.SetBool("Toward", true);
+            }
+            else
+            {
+                this.tankAnimator.SetBool("Toward", false);
+            }
         }
         else
         {
             this.tankAnimator.SetBool("Moving", false);
         }
+    }
+
+    private bool isMovingToward()
+    {
+        if (this.movingState == MovingState.ToLeft && this.faceDirection == FaceDirection.RightLeft) return true;
+        if (this.movingState == MovingState.ToRight && this.faceDirection == FaceDirection.LeftRight) return true;
+        return false;
     }
 
     public virtual void UpdateAttack()
