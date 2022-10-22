92045c3c51fff2182a78cd7129efbbadf8782592
AnhLe <lephamlananh1709@gmail.com>
2022-10-23 01:16:37 +0700

    feat: UI aiming bar va power

---
+++ Project/Assets/TestHealth.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestHealth : MonoBehaviour
+{
+    public HealthBar healthBar;
+    public int health = 0;
+
+    private void Update()
+    {
+        if (this.health != 0)
+        {
+            this.healthBar.setHealth(this.health);
+        }
+    }
+
+}
