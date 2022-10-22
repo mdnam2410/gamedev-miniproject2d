c5552381336e755f7b3081d7d9297640b35be32d
AnhLe <lephamlananh1709@gmail.com>
2022-10-23 01:34:08 +0700

    feat: UI aiming bar va power

--- Project/Assets/AngleRuler.cs
+++ Project/Assets/AngleRuler.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using static Player;
 
 public class AngleRuler : MonoBehaviour
 {
@@ -12,12 +13,21 @@ public class AngleRuler : MonoBehaviour
 
     void Start()
     {
-        
+        curAngle = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.W))
+        {
+
+        }
+    }
+
+    void IncreaseAngle(int delta)
+    {
+        curAngle += delta;
+        needle.transform.eulerAngles = new Vector3(0, 0, )
     }
 }
