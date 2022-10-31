2af9b3430d80932d7a1d70b9d3ff7992e60fd07f
Nam Mai <mdnam2410@gmail.com>
2022-10-31 13:14:13 +0700

    fix: Thiếu cờ UNITY_EDITOR gây lỗi build

--- Project/Assets/AssetPreviewExporter.cs
+++ Project/Assets/AssetPreviewExporter.cs
@@ -8,9 +8,11 @@ public class AssetPreviewExporter : MonoBehaviour
 {
     public void Export()
     {
+#if UNITY_EDITOR
         Texture2D texture = AssetPreview.GetAssetPreview(gameObject.transform);
         byte[] textureData = texture.EncodeToPNG();
 
         File.WriteAllBytes($"Assets/{gameObject.name}.png", textureData);
+#endif
     }
 }
